6fb6c1bcecd14191acf2329de1c661a2cb751d4b
HkFromMY <bpvper@gmail.com>
2023-05-03 20:03:32 +0800

    Fix Hashmap

--- dstr-assignment/HashMap.h
+++ dstr-assignment/HashMap.h
@@ -15,151 +15,171 @@
 
 using namespace std;
 
-template <typename KEY, typename VALUE>
+template <class KEY, class VALUE>
 class HashNode {
-	public:
+	public: 
 		KEY key;
 		VALUE value;
 
-		HashNode(KEY key, VALUE value) {
-			this->key = key;
-			this->value = value;
-		}
+		HashNode(KEY, VALUE);
 };
 
-template <typename KEY, typename VALUE>
-class HashMap {
-	/*
-	Hashmap greatly reduces the time complexity of searching to O(1) for best case scenario but
-	consumes large amount of memory space instead.
-	*/
+template <class KEY, class VALUE>
+HashNode<KEY, VALUE>::HashNode(KEY key, VALUE value) {
+	this->key = key;
+	this->value = value;
+}
 
-	HashNode<KEY, VALUE>** hash_arr;
-	int capacity;
-	int size;
-	HashNode<KEY, VALUE>* dummy;
+template <class KEY, class VALUE>
+class HashMap {
+	private: 
+		HashNode<KEY, VALUE>** hash_arr;
+		int capacity;
+		int size;
+		HashNode<KEY, VALUE>* dummy;
 
 	public:
-		HashMap(int capacity) {
-			// initialization of a hash array
-			this->capacity = capacity;
-			this->size = 0;
-			this->hash_arr = new HashNode<KEY, VALUE>* [capacity];
+		HashMap(int);
+		~HashMap();
 
-			for (int i = 0; i < capacity; i++) {
-				this->hash_arr[i] = NULL;
-			}
+		unsigned long int hashCode(string);
+		void insertNode(KEY, VALUE);
+		VALUE get(string);
+		void incrementCount(string);
+		int getSize();
+		void displayAllHashNodes();
+		LinkedList<Counter>* to_counters();
+};
 
-		}
+template <class KEY, class VALUE>
+HashMap<KEY, VALUE>::HashMap(int capacity) {
+	// initialization of a hash array
+	this->capacity = capacity;
+	this->size = 0;
+	this->hash_arr = new HashNode<KEY, VALUE>* [capacity];
 
-		unsigned long hashCode(string key) {
-			// this function hash the key in string format, then returning a hash to search for element in hash array
+	for (int i = 0; i < capacity; i++) {
+		this->hash_arr[i] = NULL;
+	}
+}
+
+template <class KEY, class VALUE>
+HashMap<KEY, VALUE>::~HashMap() {}
+
+template <class KEY, class VALUE>
+unsigned long HashMap<KEY, VALUE>::hashCode(string key) {
+	// this function hash the key in string format, then returning a hash to search for element in hash array
 			// algorithm source: https://gist.github.com/MohamedTaha98/ccdf734f13299efb73ff0b12f7ce429f
 
 			// only accepts string as key values
-			unsigned long hash = 5381L;
-			int c;
-			for (int i = 0; i < key.length(); i++) {
-				c = key[i];
-				hash = ((hash << 5) + hash) + c; // shift the hash's values by bits then plus c
-			}
+	unsigned long hash = 5381L;
+	int c;
+	for (int i = 0; i < key.length(); i++) {
+		c = key[i];
+		hash = ((hash << 5) + hash) + c; // shift the hash's values by bits then plus c
+	}
 
-			return hash % capacity;
-		}
+	return hash % capacity;
+}
 
-		void insertNode(KEY key, VALUE value) {
-			HashNode<KEY, VALUE>* temp = new HashNode<KEY, VALUE>(key, value);
+template <class KEY, class VALUE>
+void HashMap<KEY, VALUE>::insertNode(KEY key, VALUE value) {
+	HashNode<KEY, VALUE>* temp = new HashNode<KEY, VALUE>(key, value);
 
-			int hashIndex = hashCode(key);
+	int hashIndex = hashCode(key);
 
-			while (
-				hash_arr[hashIndex] != NULL &&
-				hash_arr[hashIndex]->key != key
-			) {
-				// simple mechanism by incermenting hash by 1 to insert duplicated hash (prevent hash collision problem)
-				hashIndex++;
-				hashIndex %= capacity;
-			}
+	while (
+		hash_arr[hashIndex] != NULL &&
+		hash_arr[hashIndex]->key != key
+		) {
+		// simple mechanism by incermenting hash by 1 to insert duplicated hash (prevent hash collision problem)
+		hashIndex++;
+		hashIndex %= capacity;
+	}
 
-			if (
-				hash_arr[hashIndex] == NULL
-			) {
-				size++;
-			}
-			hash_arr[hashIndex] = temp;
-		}
+	if (
+		hash_arr[hashIndex] == NULL
+		) {
+		size++;
+	}
+	hash_arr[hashIndex] = temp;
+}
 
-		VALUE get(string key) {
-			int hashIndex = hashCode(key);
-			int counter = 0;
+template <class KEY, class VALUE>
+VALUE HashMap<KEY, VALUE>::get(string key) {
+	int hashIndex = hashCode(key);
+	int counter = 0;
 
-			while (hash_arr[hashIndex] != NULL) {
-				if (counter++ > capacity) return NULL;
+	while (hash_arr[hashIndex] != NULL) {
+		if (counter++ > capacity) return NULL;
 
-				if (hash_arr[hashIndex]->key == key) return hash_arr[hashIndex]->value;
+		if (hash_arr[hashIndex]->key == key) return hash_arr[hashIndex]->value;
 
-				// simple mechanism to trace back if the target is a duplicated hash (prevent hash collision problem)
-				hashIndex++;
-				hashIndex %= capacity;
-			}
+		// simple mechanism to trace back if the target is a duplicated hash (prevent hash collision problem)
+		hashIndex++;
+		hashIndex %= capacity;
+	}
 
-			return NULL;
-		}
+	return NULL;
+}
 
-		void incrementCount(string key) {
-			/*
+template <class KEY, class VALUE>
+void HashMap<KEY, VALUE>::incrementCount(string key) {
+	/*
 			This is a specialized function to keep track of the occurrences of a value such as University
 			If the key does not exist in this hash table, then a new key with value of 1 is added to the hash table.
 			*/
-			int hashIndex = hashCode(key);
-			int counter = 0;
+	int hashIndex = hashCode(key);
+	int counter = 0;
 
-			while (hash_arr[hashIndex] != NULL) {
-				if (counter++ > capacity) return;
+	while (hash_arr[hashIndex] != NULL) {
+		if (counter++ > capacity) return;
 
-				if (hash_arr[hashIndex]->key == key) {
-					hash_arr[hashIndex]->value += 1;
-					return;
-				}
-
-				// simple mechanism to trace back if the target is a duplicated hash
-				hashIndex++;
-				hashIndex %= capacity;
-			}
-
-			// if the key does not exists, then add a new key and use 1 as initial value
-			insertNode(key, 1);
+		if (hash_arr[hashIndex]->key == key) {
+			hash_arr[hashIndex]->value += 1;
+			return;
 		}
 
-		int getSize() {
-			return size;
+		// simple mechanism to trace back if the target is a duplicated hash
+		hashIndex++;
+		hashIndex %= capacity;
+	}
+
+	// if the key does not exists, then add a new key and use 1 as initial value
+	insertNode(key, 1);
+}
+
+template <class KEY, class VALUE>
+int HashMap<KEY, VALUE>::getSize() {
+	return size;
+}
+
+template <typename KEY, typename VALUE>
+void HashMap<KEY, VALUE>::displayAllHashNodes() {
+	for (int i = 0; i < capacity; i++) {
+		if (hash_arr[i] != NULL) {
+			cout << "key = " << hash_arr[i]->key << "  value = " << hash_arr[i]->value << endl;
 		}
+	}
+}
 
-		void displayAllHashNodes() {
-			for (int i = 0; i < capacity; i++) {
-				if (hash_arr[i] != NULL) {
-					cout << "key = " << hash_arr[i]->key << "  value = " << hash_arr[i]->value << endl;
-				}
-			}
+template <typename KEY, typename VALUE>
+LinkedList<Counter>* HashMap<KEY, VALUE>::to_counters() {
+	// this functions is to convert all the key-value pairs in this hash table to a list of Counter object
+	// so that the occurrences can be sorted
+	// time complexity - O(N)
+	LinkedList<Counter>* counters = new LinkedList<Counter>();
+
+	for (int i = 0; i < capacity; i++) {
+		if (hash_arr[i] != NULL) {
+			counters->appendNewNode(new Counter(
+				hash_arr[i]->key,
+				hash_arr[i]->value
+			));
 		}
+	}
 
-		LinkedList<Counter>* to_counters() {
-			// this functions is to convert all the key-value pairs in this hash table to a list of Counter object
-			// so that the occurrences can be sorted
-			// time complexity - O(N)
-			LinkedList<Counter>* counters = new LinkedList<Counter>();
-
-			for (int i = 0; i < capacity; i++) {
-				if (hash_arr[i] != NULL) {
-					counters->appendNewNode(new Counter(
-						hash_arr[i]->key,
-						hash_arr[i]->value
-					));
-				}
-			}
-
-			return counters;
-		}
-};
+	return counters;
+}
 
 #endif // !hashmap_h
